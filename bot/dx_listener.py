ce37feb59355632c1d4b0e44be874bab6c817de2
Weyand <60848866+Weyandp@users.noreply.github.com>
2025-08-06 00:54:47 +0200

    Update dx_listener.py

--- bot/dx_listener.py
+++ bot/dx_listener.py
@@ -1,8 +1,8 @@
 import socket, requests, time, sqlite3, os
 
 DISCORD_WEBHOOK = os.getenv("https://discord.com/api/webhooks/1402410297158799592/BdirE10g4jz4hrgolOodvr_hCUwfPYkgOcGvBIKG9ofTCz_Mc4lj2xIUmfz_sDSrrxD2")
-DX_CLUSTER_HOST = os.getenv("DX_CLUSTER_HOST", "dxc.kb8zgl.net")
-DX_CLUSTER_PORT = int(os.getenv("DX_CLUSTER_PORT", "7373"))
+DX_CLUSTER_HOST = os.getenv("dxc.kb8zgl.net", "dxc.kb8zgl.net")
+DX_CLUSTER_PORT = int(os.getenv("7373", "7373"))
 CALLSIGN = os.getenv("CALLSIGN", "N0CALL")
 
 def db_connect():
